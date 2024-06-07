2e195fea358671997cca2253010f862da71a26a2
unknown <guthemberg.interaminense@gmail.com>
2024-06-06 23:33:08 -0300

    feat: criacao de esqueleto de projeto

---
+++ src/test/java/com/carteiraleve/api/CarteiraLeveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carteiraleve.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarteiraLeveApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
